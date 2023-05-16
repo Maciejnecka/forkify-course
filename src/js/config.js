f2e7508b7d6796675806797e0313c01aeeb3f2eb
Maciejnecka <maciekmck209@gmail.com>
2023-05-16 10:53:39 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '281c8acb-196a-4b3d-818f-9bd4f3b535f4';
+export const MODAL_CLOSE_SEC = 2.5;
